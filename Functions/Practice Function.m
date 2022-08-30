10480114bc13555a2a2e962b39762b7aac87f3c0
VU QUANG NGUYEN <vuquangnguyen2018@gmail.com>
2022-08-30 19:47:32 +0700

    update

--- Functions/Practice Function.m
+++ Functions/Practice Function.m
@@ -1,4 +1,36 @@
+#-----------------------------------############ ------------------------------------------------
+# Practice Activity 18 
+
+
+#DIM CUSTOMER 
+
+FullName = DimCustomer[Title] & " " & DimCustomer[FirstName] & " " & DimCustomer[MiddleName] & " " & DimCustomer[LastName]
+NoMiddleName = if(ISBLANK(DimCustomer[MiddleName]),"No Middle Name", BLANK()) 
+#HOUSE AND CAR 
+HasBothHouseAndCar = IF(AND(DimCustomer[HouseOwnerFlag]>=1,DimCustomer[NumberCarsOwned]>=1),"YES", "NO") 
+
+HasBothHouseAndCar = IF(AND(DimCustomer[HouseOwnerFlag]>=1,DimCustomer[NumberCarsOwned]>=1),"YES", 
+IF(DimCustomer[NumberCarsOwned]>=1, "Car Only", 
+IF(DimCustomer[HouseOwnerFlag]>=1, "House Only", "Neither" ) ))
+
+# FACT SALES 
+
+Season = SWITCH(FactInternetSales[QuarterNumber],
+1, "SPRING",
+2, "SUMMER",
+3, "AUTUMN",
+4, "WINTER",
+"ERROR")
+
+# DIM SALES TERRITORY 
+InUS = IF(DimSalesTerritory[SalesTerritoryCountry] = "United States", "In US", "Outside of US") 
+
+
+
+#-----------------------------------############ ------------------------------------------------
 # Practice Activity 20
+
+
 IncomePerPerson=MROUND(DimCustomer[Yearly]/DimCustomer[NumberPeople],1000)
 RankEQ = RANK.EQ(DimCustomer[IncomePerPerson],DimCustomer[IncomePerPerson],DESC)
 Hundreds=QUOTIENT(DimCustomer[RankEQ],100) 
